2bb91b466fa7041ffa7adc0e1a69fc48e3264439
zeldacornelia <zeldacornelias@gmail.com>
2026-02-13 12:48:58 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+function NumberYay () {
+    list = [
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    ]
+}
+input.onGesture(Gesture.Shake, function () {
+    Rolllll()
+})
+function Rolllll () {
+    list[randint(0, 5)].showImage(0)
+}
+let list: Image[] = []
+NumberYay()
+Rolllll()
